1c42517a83573ebb7147e3bedca1791f04ccb0b8
shaswat-indian <shaswat.dba@gmail.com>
2018-04-25 00:54:43 +0530

    Added Icons and Youtube link

--- app/src/main/java/myapps/com/pbl/FirstFragment.java
+++ app/src/main/java/myapps/com/pbl/FirstFragment.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by shaswat on 9/3/18.
@@ -32,5 +35,11 @@ public class FirstFragment extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+
+        TextView textView =(TextView)findViewById(R.id.textView2);
+        textView.setClickable(true);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        String text = "<a href='https://www.youtube.com/watch?v=6ZfuNTqbHE8'> Click Here to Watch our Tutorial Video! </a>";
+        textView.setText(Html.fromHtml(text));
     }
 }
